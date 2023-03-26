09a823ce8d609cb7c22e76abc5ee228677712467
Michael Parker <michaelrbparker@gmail.com>
2023-03-26 16:35:29 +0100

    https://youtu.be/qIbuVhwRNpg

--- sorting/insertionsort.py
+++ sorting/insertionsort.py
@@ -1,14 +1,15 @@
-#video https://youtu.be/dPfNrVi_Oj4
+#video https://youtu.be/qIbuVhwRNpg
 
 listtest = [2,6,4,12,89,3,69]
+
 def Insertion_Sort(arr):
     for i in range(1,len(arr)):
         key = arr[i]
         j = i -1
         while j >=0 and key < arr[j]:
             arr[j+1] = arr[j]
-            j -= 1
+            j -=1
         arr[j+1] = key
-    return arr 
+    return arr
 
 print(Insertion_Sort(listtest))
