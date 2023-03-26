55712ff9b5f3200f2031dcc5c641d9c9407ee835
Michael Parker <michaelrbparker@gmail.com>
2023-03-26 15:49:38 +0100

    Insertion_Sort

---
+++ sorting/insertionsort.py
@@ -0,0 +1,12 @@
+listtest = [2,6,4,12,89,3,69]
+def Insertion_Sort(arr):
+    for i in range(1,len(arr)):
+        key = arr[i]
+        j = i -1
+        while j >=0 and key < arr[j]:
+            arr[j+1] = arr[j]
+            j -= 1
+        arr[j+1] = key
+    return arr 
+
+print(Insertion_Sort(listtest))
